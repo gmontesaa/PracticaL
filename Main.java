1dd9c720849749dc929d0068c01c562553536844
gmontesaa <158535358+gmontesaa@users.noreply.github.com>
2024-03-21 21:32:30 -0500

    Update Main.java

--- Main.java
+++ Main.java
@@ -7,12 +7,41 @@ public class Main {
     public static void main(String[] args){
         Human LuisCarlos = new Human();
         Dog Lucas = new Dog();
-        LuisCarlos.changeDimension();
-        // Lucas.changeDimension(); // Lucas is a dog, it couldn't travel across dimentions!
         Bird Piolin = new Bird();
-        Piolin.changeDimension();
+
+        System.out.println("Human Actions");
+        LuisCarlos.born();
+        LuisCarlos.grow();
+        LuisCarlos.reproduce();
+        LuisCarlos.die();
+        LuisCarlos.respawn();
+        LuisCarlos.fly();
+        LuisCarlos.changeDimension();
+
+
+        System.out.println("Dog Actions");
+        Lucas.born();
+        Lucas.grow();
+        Lucas.reproduce();
+        Lucas.die();
+        Lucas.play();
+        Lucas.sleep();
+
+
+        System.out.println("Bird Actions");
+        Piolin.born();
+        Piolin.grow();
+        Piolin.reproduce();
+        Piolin.die();
         Piolin.respawn();
+        Piolin.fly();
+        Piolin.changeDimension();
+
+
+        System.out.println("Creating Living Beings");
         LivingBeingCreator creator = new LivingBeingCreator();
         creator.createLivingBeings();
     }
 }
+
+
